43a89eed96b9d9ea4c77a0b8c2e230582ffdba6f
dlanaras <70207362+dlanaras@users.noreply.github.com>
2021-02-01 12:46:41 +0100

    added params as suggested by the php sniffer

--- person.class.php
+++ person.class.php
@@ -1,8 +1,14 @@
 <?php 
-/** 
-I'm aware that cloning the phpcs and phpcbf isn't efficient 
-(need to clone it for every project) and is only a temporary solution 
- **/
+ /** 
+  * I'm aware that cloning the phpcs and phpcbf isn't efficient 
+  * (need to clone it for every project) and is only a temporary solution
+  *
+  * @category Idk
+  * @package  Idk
+  * @author   dlanaras <dimitrislanaras04@outlook.com>
+  * @license  none
+  * @link     https://github.com/dlanaras/timescheduler/tree/master
+  */
 
 class Person
 {

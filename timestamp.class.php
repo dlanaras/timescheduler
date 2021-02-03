83bfb3877fb6cc2ae6c35226856920fcd32ba64e
dlanaras <70207362+dlanaras@users.noreply.github.com>
2021-02-03 13:49:50 +0100

    creates file of person name and saves timestamp

--- timestamp.class.php
+++ timestamp.class.php
@@ -26,7 +26,30 @@ class Stempel
      */
     public function __setStamp() 
     {
-        //Does something
+        //checks if name entered is in array and creates file out of the name also adds timestamp in file
+        $answer = readline("Wie heissen Sie: ");
+        $project = readline("Woran arbeiten Sie jetzt? ");
+        $stamp = new Stempel(date("h:i:s"), date("h:i:s"), $project);
+        $jsonstamp = json_encode($stamp, true);
+        $handle = file_get_contents("./data.json", "r");
+        //if stamp isset on start and isset on project -> those 2 arent isset and end isset
+        $data = json_decode($handle, true);
+        
+        foreach($data['Person'] as $result) {
+        if($answer == $result['vorname']) {
+          $filename = $result['vorname'] . ".json";
+          $conn = fopen($filename, "a+");
+          $stat = fstat($conn);
+          ftruncate($conn, $stat['size']-2);
+          fwrite($conn, "," . "\n" . $jsonstamp);
+          fwrite($conn, "\n]}");
+          fclose($conn);
+        } 
+
+        //For now the timestamps in the newly created files cannot be printed out due to wrong formating of json
+      }
+
+        print_r($stamp);
     }
 
     public function __construct($start, $end, $project) 
@@ -38,14 +61,16 @@ class Stempel
 
 }
 
-//It's going to be turned into a function soon 
-$project = readline("Woran arbeiten Sie jetzt? ");
+$bigtest = new Stempel("", "", "");
+$bigtest->__setStamp();
 
-$testtime = mktime(03, 56, 15);
-$stamp = new Stempel(date("h:i:s"), date("h:i:s", $testtime), $project);
+//It's going to be turned into a function soon 
+
+
+
 //$sub = strtotime($stamp->$end) - strtotime($stamp[$start]);
 
 //echo $worktime = Stempel[start] - Stempel[end] . "\n";
-print_r($stamp);
+
 
 //print_r($sub);
